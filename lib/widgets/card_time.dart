c038935f3d7d57ffd14913f444b663f40fd88554
crishard <crislantorres9@gmail.com>
2022-07-10 09:35:30 -0300

    criando class CardTime

---
+++ lib/widgets/card_time.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class CardTime extends StatelessWidget{
+  const CardTime({Key? key, required this.time, required this.escudo}) : super(key: key);
+  final String time;
+  final Image escudo;
+
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.blue[200],
+      child: Row(
+        
+      ),
+    );
+  }
+}
