1b500e477c4af677a941f572e28d8eb7a25b169a
crishard <crislantorres9@gmail.com>
2022-07-16 00:48:47 -0300

    adiconando opção null

--- lib/widgets/card_time.dart
+++ lib/widgets/card_time.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class CardTime extends StatelessWidget{
   const CardTime({Key? key, required this.time, required this.escudo}) : super(key: key);
-  final String time;
-  final String escudo;
+  final String? time;
+  final String? escudo;
 
   Widget build(BuildContext context){
     return Container(
@@ -14,8 +14,8 @@ class CardTime extends StatelessWidget{
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(time, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),),
-          Image.network(escudo, width: 60, height: 60,),
+          Text(time!, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),),
+          Image.network(escudo!, width: 60, height: 60,),
         ],
       ),
     );
