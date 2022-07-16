4925e4c2cdb7f816edd9b25e1e4976186fa72634
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-07-16 17:23:00 -0300

    Criação das listas para teste - Card Resultados

---
+++ lib/widgets/card_resultados.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CardResultados extends StatelessWidget {
+  const CardResultados({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+  // Escudos teste
+
+  List<String> escudosMandantes = ["https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png", "https://upload.wikimedia.org/wikipedia/en/5/5f/Clube_Atl%C3%A9tico_Mineiro_crest.svg"];
+
+  List<String> escudosVisitantes = ["https://static.wikia.nocookie.net/captaintsubasa/images/4/46/S%C3%A3o_Paulo_FC.png/revision/latest?cb=20180721104532", "https://upload.wikimedia.org/wikipedia/commons/6/60/CruzeiroECMG.png"];
+
+  List<String> testeEscudo = ["https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png", "https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png", "https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png", "https://upload.wikimedia.org/wikipedia/pt/b/b4/Corinthians_simbolo.png",];
+
+  // Nomes teste
+
+  List<String> nomesDosMandantes = ["CORINTHIANS", "ATLÉTICO MINEIRO"]; 
+
+  List<String> nomesDosVisitantes = [ "SÃO PAULO", "CRUZEIRO"];
+
+
+  // Gols teste
+
+  List<String> golsDosMandantes = ["2", "1",];
+
+  List<String> golsDosVisitantes = ["1", "0"];
+    return Container();
+  }
+}
