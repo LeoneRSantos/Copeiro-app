33f63dcd148b1a6cb09a3cef6207e621e8b3d514
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2022-07-24 21:23:48 -0300

    alterando repositorio de time e lista de partidas

--- lib/repositorio/repositorio_de_lista_de_partida.dart
+++ lib/repositorio/repositorio_de_lista_de_partida.dart
@@ -3,18 +3,18 @@ import 'package:dio/dio.dart';
 import '../modelos/modelo_lista_de_partidas.dart';
 
 abstract class Repositorio<T> {
-  Future<List<T>> getAll();
+  Future<List<T>> getAll(int num);
   Dio dio = Dio();
 }
 
 class RepositorioListaDePartida extends Repositorio<PartidaUnica> {
   @override
-  Future<List<PartidaUnica>> getAll() async {
+  Future<List<PartidaUnica>> getAll(int num) async {
     Response resposta;
 
     try {
       resposta = await dio.get(
-          'https://app.sportdataapi.com/api/v1/soccer/matches?apikey=143cd3d0-fbc2-11ec-bf95-532befddefc2&season_id=3009&date_to=2022-07-09');
+          'https://app.sportdataapi.com/api/v1/soccer/matches?apikey=143cd3d0-fbc2-11ec-bf95-532befddefc2&season_id=$num&date_to=2022-07-09');
       if (resposta.statusCode == 200) {
         return ListaDePartidas.fromJson(resposta.data).data!;
       }
