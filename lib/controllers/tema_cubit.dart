d4d98e4d2f2f3b24bbbf18c2f48292791798343c
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-08-01 23:18:37 -0300

    Alteração de tema escuro para tema claro como padrão

--- lib/controllers/tema_cubit.dart
+++ lib/controllers/tema_cubit.dart
@@ -6,7 +6,7 @@ class TemaCubit extends Cubit<ThemeData> {
   static final _temaClaro = DesafioTheme.lightTheme;
   static final _temaEscuro = DesafioTheme.darkTheme;
 
-  TemaCubit() : super(_temaEscuro);
+  TemaCubit() : super(_temaClaro);
 
   void alteraTema() => state.brightness == Brightness.light
       ? emit(_temaEscuro)
