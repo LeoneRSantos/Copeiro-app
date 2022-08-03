919a9d329fe46bc096db8826dcad54ca4e326d2e
Musa Sobripin <moisespinhao2001@gmail.com>
2022-08-02 21:34:07 -0300

    atualização das telas de home page e informações de paises

---
+++ lib/models/text/text_infos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextInfos extends StatelessWidget{
+ const TextInfos({ Key? key, required this.name}) : super(key: key);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme esquemaDeCores = Theme.of(context).colorScheme;
+    return Text(name, 
+        style: TextStyle(
+        fontFamily: 'TekoRegular',
+        fontSize: 20,
+        color: esquemaDeCores.primary,
+        ),);
+  }
+}
