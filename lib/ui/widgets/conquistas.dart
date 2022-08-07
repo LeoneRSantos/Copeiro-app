e9833c8e1843761e35ead5405ee5959a7b77d405
crishard <crislantorres9@gmail.com>
2022-08-06 21:20:52 -0300

    Refatoração dos widgets de conquistas

--- lib/ui/widgets/conquistas.dart
+++ lib/ui/widgets/conquistas.dart
@@ -1,16 +1,17 @@
-import 'package:copeiro_app/models/text/text_infos.dart';
-import 'package:copeiro_app/models/text/text_pais.dart';
+import 'package:copeiro_app/ui/widgets/help_informacoes.dart';
 import 'package:flutter/material.dart';
 
 class Conquistas extends StatelessWidget {
-  Conquistas({Key? key, 
-  required this.pais, 
-  required this.copa, 
-  required this.copaContinental, 
-  required this.copaDasConfederacoes, 
-  required this.olimpiadas, 
-  required this.copaDoMundoFeminina, 
-  required this.imagem}) : super(key: key);
+  Conquistas(
+      {Key? key,
+      required this.pais,
+      required this.copa,
+      required this.copaContinental,
+      required this.copaDasConfederacoes,
+      required this.olimpiadas,
+      required this.copaDoMundoFeminina,
+      required this.imagem})
+      : super(key: key);
 
   final String pais;
   final String copa;
@@ -22,28 +23,58 @@ class Conquistas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column( 
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [ 
-        Row( 
-          children: [ 
-            Image.asset(imagem, width: 70.0, height: 70.0,), 
-            TextPais(name: pais), 
-          ],
-        ), 
-        Center(
-          child: Column( 
-            children: [ 
-              TextInfos(name: copa),
-              TextInfos(name: copaContinental),
-              TextInfos(name: copaDasConfederacoes),
-              TextInfos(name: olimpiadas),
-              TextInfos(name: copaDoMundoFeminina),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                imagem,
+                width: 70.0,
+                height: 70.0,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                pais,
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: "TekoRegular"),
+              ),
             ],
           ),
-        ),
-      ],
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${copa} Títulos da Copa do Mundo de Futebol",
+                style: TextInformacoes,
+              ),
+              Text(
+                "${copaContinental} Títulos de Copa América",
+                style: TextInformacoes,
+              ),
+              Text(
+                "${copaDasConfederacoes} Títulos de Copa das Confederações",
+                style: TextInformacoes,
+              ),
+              Text(
+                "${olimpiadas} Medalhas de Ouro Olímpicas",
+                style: TextInformacoes,
+              ),
+              Text(
+                "${copaDoMundoFeminina} Copa do Mundo Feminina",
+                style: TextInformacoes,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
