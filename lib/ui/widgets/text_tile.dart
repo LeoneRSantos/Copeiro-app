7b4f8f99d6ff3682243d07bde47c2e143853280b
crishard <crislantorres9@gmail.com>
2022-08-06 21:24:56 -0300

    Refatoração dos widgets de text_tile e card_resultados

--- lib/ui/widgets/text_tile.dart
+++ lib/ui/widgets/text_tile.dart
@@ -8,18 +8,15 @@ class TextTile extends StatelessWidget{
   Widget build(BuildContext context){
     ColorScheme esquemaDeCores = Theme.of(context).colorScheme;
     
-    return Container(
-      margin: const EdgeInsets.only(top: 20,),
-      child: Row(
+    return Row(
       children: [
         IconButton(
           icon: Icon(Icons.arrow_back_ios_new, size: 30, color: esquemaDeCores.primary,),
           onPressed: () => Navigator.of(context).pop(),
         ), 
         SizedBox(width: 10,),
-        Text(liga, style: TextStyle(fontSize: 35, fontFamily: 'TekoRegular', color: esquemaDeCores.primary),),
+        Text(liga, style: TextStyle(fontSize: 32, fontFamily: 'TekoRegular', color: esquemaDeCores.primary),),
       ],
-    ),
     );
     
   }
